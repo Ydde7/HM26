5dfe2285fb2d5b90df58191363b694eec2544349
TheodoreHo <thehofamily2020@gmail.com>
2026-03-07 17:18:57 -0800

    In Progress

--- Backend/main.py
+++ Backend/main.py
@@ -12,30 +12,46 @@ def hi():
 #     outputs=["text"]
 
 # )
-def onClick(){
-
-
-}
-
 
 with gr.Blocks() as main:
     with gr.Row():
-        with gr.Column(scale=0.5): # scale relates to proportion of screen taken up
+        with gr.Column(scale=1): # scale relates to proportion of screen taken up
             bt1 = gr.Button("Critial")
             bt2 = gr.Button("Moderate")
-            bt3 = gr.Button("Safe")
+            bt3 = gr.Button("Safe", visible=False)
             bt4 = gr.Button("Discussion")
 
-            bt1.click(
-                fn=onClick,
-                inputs= ["bt1"],
-                outputs=["md1"]
+            back = gr.Button("Back", visible="hidden")
 
-            )
+
         with gr.Column(scale=2):
             md1 = gr.Markdown("")
         with gr.Column(scale=2):
             btn1 = gr.Button("Button 1")
             btn2 = gr.Button("Button 2")
+
+
+        def visual(value: str | None):
+
+            if (bt2.visible == False):
+                return {
+                    bt2: gr.Button(visible=True),
+                    md1: gr.Markdown("We are back!")
+                }
+            else:
+                bt2.visible = False
+                return {
+                    bt2: gr.Button(visible=False),
+                    bt3: gr.Button(visible=True),
+                    md1: gr.Markdown(str(bt2.visible))
+                }
+
+
+
+        bt1.click(
+            fn=visual,
+            inputs= [bt1],
+            outputs=[md1, bt2, bt3]
+        )
 
 main.launch()
