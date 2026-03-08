ba9f1f99b25e7e4314c321d7302a53e5a7cf540a
TheodoreHo <thehofamily2020@gmail.com>
2026-03-07 16:13:25 -0800

    Initial commit

--- Backend/main.py
+++ Backend/main.py
@@ -1,13 +1,41 @@
 import gradio as gr
+import random
 
-def greet(name, intensity):
-    return "Hello, " + name + "!" * int(intensity)
 
-demo = gr.Interface(
-    fn=greet,
-    inputs=["text", "slider"],
-    outputs=["text"],
-    api_name="predict"
-)
+# print(gr.__version__);
+def hi():
+    print("Welcome back!")
 
-demo.launch()
+# demo = gr.Interface(
+#     fn = hi;
+#     inputs= ["text"]
+#     outputs=["text"]
+
+# )
+def onClick(){
+
+
+}
+
+
+with gr.Blocks() as main:
+    with gr.Row():
+        with gr.Column(scale=0.5): # scale relates to proportion of screen taken up
+            bt1 = gr.Button("Critial")
+            bt2 = gr.Button("Moderate")
+            bt3 = gr.Button("Safe")
+            bt4 = gr.Button("Discussion")
+
+            bt1.click(
+                fn=onClick,
+                inputs= ["bt1"],
+                outputs=["md1"]
+
+            )
+        with gr.Column(scale=2):
+            md1 = gr.Markdown("")
+        with gr.Column(scale=2):
+            btn1 = gr.Button("Button 1")
+            btn2 = gr.Button("Button 2")
+
+main.launch()
